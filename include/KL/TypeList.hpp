6e909cc5bb0f84b6287b010271b605067ecf8bdc
Alain Martin <amartin@keytrol.org>
2015-01-30 22:08:20 +0100

    Remove unused template parameters

--- include/KL/TypeList.hpp
+++ include/KL/TypeList.hpp
@@ -59,10 +59,10 @@ public:
 
     using Empty = std::true_type;
 
-    template <typename Element>
+    template <typename>
     using Contains = std::false_type;
 
-    template <typename Element, typename ValueType = size_type>
+    template <typename, typename ValueType = size_type>
     using Count = std::integral_constant<ValueType, 0>;
 
     template <typename Element>
